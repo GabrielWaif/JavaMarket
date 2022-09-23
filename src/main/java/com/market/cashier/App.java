9fafe107b4ebc46a803aa10df4ea2b580c3e4dfe
Gabriel Rosa <gabrielrosarcg@hotmail.com>
2022-09-23 12:20:07 -0300

    change test values

--- src/main/java/com/market/cashier/App.java
+++ src/main/java/com/market/cashier/App.java
@@ -6,6 +6,7 @@ public class App
     {
         Products products = new Products();
         ShoppingCart sc = new ShoppingCart(products);
+        products.addProduct("Milk", "1 galon", 10.0);
         sc.addCart(10, 10);
         sc.addCart(10, 1);
         sc.showCart();
