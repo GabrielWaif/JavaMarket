63856c3ec447a5a45e7b24d95b5ac5f0a0ef952d
Gabriel Rosa <gabrielrosarcg@hotmail.com>
2022-09-24 22:47:40 -0300

    added comments through the app functions

--- src/main/java/com/market/cashier/App.java
+++ src/main/java/com/market/cashier/App.java
@@ -5,18 +5,25 @@ import java.util.function.BiConsumer;
 
 public class App {
 
+  //Class instances of the created objects
   private static Products products = new Products();
-  private static Scanner scanner = new Scanner(System.in);
   private static ShoppingCart shoppingCart = new ShoppingCart(products);
+
+  //Global scannner object
+  private static Scanner scanner = new Scanner(System.in);
 
   public static void main(String[] args) {
     String userInput = "";
+
+    //Loop that reads the terminal input that is used to choosee the menu(owner/cart) until he exits.
     do {
       System.out.print("Market/> ");
       userInput = scanner.nextLine();
       if (!userInput.equals("exit")) {
+        //Storage the user input in args[]
         String[] inputs = userInput.split(" ");
         if (inputs[0].equals("use")) {
+          //Based in the "use ..." calls the "terminalCommands" function passing an Lambda with the commands for that choice.
           switch (inputs[1]) {
             case "owner":
               terminalCommands(
@@ -50,6 +57,7 @@ public class App {
                 "Owner"
               );
               break;
+
             case "cart":
               terminalCommands(
                 (String[] commands, String command) -> {
@@ -71,12 +79,12 @@ public class App {
     } while (!userInput.equals("exit"));
   }
 
-  static void ownerCommands() {}
-
+  //Prints the user input telling him that it isn't a valid command
   static void invalidCommand(String command) {
     System.out.println(command + " is not a valid command!");
   }
 
+  //Loops through the user input calling the commands based in the Consumer passed through the main function
   static void terminalCommands(
     BiConsumer<String[], String> func,
     String terminalPath
@@ -87,7 +95,7 @@ public class App {
       command = scanner.nextLine();
       String[] args = command.split(" ");
 
-      if (!command.equals("exit")) {
+      if (!command.equals("exit") && args.length > 0) {
         func.accept(args, command);
       } else System.out.println(
         "exiting " + terminalPath.toLowerCase() + "..."
